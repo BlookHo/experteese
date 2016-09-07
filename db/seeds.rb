fc199f06c474acdaeaa697f5f1f7789b4ec75143
Aleksei <zoneiva@gmail.com>
2016-09-07 14:18:20 +0300

    seeds.rb with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Image.delete_all
+# Image.reset_pk_sequence
+Image.create([
+
+                 {name: 'Ball_Renour.jpg', path: '//hg7656yy'},
+                 {name: 'Picasso Fabrique.jpg', path: '//dd hdfbhdfyy'},
+                 {name: 'dance_mattis2.jpg', path: '//dfgvdfdfg'},
+                 {name: 'raduga5обрез.jpg', path: '//rrbfbfhg7656yy'},
+                 {name: 'Кандинский_ж.к.г_авапг.jpg', path: '//ka dfgvdfdfg'},
+                 {name: 'Рейно_Ночь_класс.jpg', path: '//rrrc dfgvdfdfg'},
+                 {name: 'Серый - середина - стандарт.jpg', path: '//cc hg7656yy'},
+                 {name: 'Черно-Белый - стандарт.jpg', path: '//cvcv dfgvdfdfg'},
+                 {name: 'Черный лист - стандарт.jpg', path: '//chchvbvb hg7656yy'},
+                 {name: 'ван_гог_куст2.jpg', path: '//vsdas'}
+
+             ])
