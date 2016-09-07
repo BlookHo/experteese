9cb14516c27c1e631de53025154ec19b266bee99
Aleksei <zoneiva@gmail.com>
2016-09-07 15:41:18 +0300

    Users & Microposts - associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  
+  has_many :microposts
+  
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }
+
 end
