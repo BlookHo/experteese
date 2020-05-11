ade65107e8c7bdd178de639716abef53117abda9
Aleksei Ivanishchev <zoneiva@gmail.com>
2020-05-11 14:44:20 +0300

    - asset precompile RegEx

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.paths << Rails.root.join("assets", "images", "pi
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(
-  /assets/pictures/*.jpg
-)
+# Rails.application.config.assets.precompile += %w(
+#   /assets/pictures/*.jpg
+# )
