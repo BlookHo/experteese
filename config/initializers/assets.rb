d71f3428e2a005a9758ae230dee2d00cc6808275
Aleksei Ivanishchev <zoneiva@gmail.com>
2020-05-11 14:37:50 +0300

    - assets.compile = false

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,5 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join("assets", "images", "pi
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(
-  /assetspictures/*.jpg
+  /assets/pictures/*.jpg
 )
